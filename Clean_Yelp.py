72fd1c0f140f96ef085c21b478e26b831d00ee44
Ella Zhang <yz850@georgetown.edu>
2020-11-30 21:45:22 -0500

    Cleaned DC_Yelp

--- Clean_Yelp.py
+++ Clean_Yelp.py
@@ -1,5 +1,6 @@
 # Using SQL + Pandas
 import pandas as pd
+import numpy as np
 import sqlite3
 
 # Data Visualization
@@ -49,9 +50,44 @@ DC_Yelp = DC_Yelp.sort_values('index').drop('index',axis=1).reset_index(drop=Tru
 DC_Yelp
 
 
+# Remove "star rating" in the rating column
+DC_Yelp['rating'] = DC_Yelp.rating.str.rstrip(' star rating')
+
+# Convert rating to float
+DC_Yelp['rating'] =  DC_Yelp['rating'].astype(float)
+
+DC_Yelp
 
 
+# Convert review_count to int
+DC_Yelp['review_count'] =  DC_Yelp['review_count'].astype(int)
 
+
+# Split price_category into two columns
+DC_Yelp[['price_range','category']] = DC_Yelp.price_category.str.rsplit("$",1,expand=True)
+DC_Yelp = DC_Yelp.drop('price_category',axis=1)
+
+DC_Yelp
+
+
+# Save the indices of NaN in category
+swap = DC_Yelp.index[DC_Yelp['category'].isna()]
+
+# Fill NaN in category with price_range
+DC_Yelp['category'] = DC_Yelp['category'].fillna(DC_Yelp['price_range'])
+
+# Add back one $ to price_range
+DC_Yelp['price_range'] = DC_Yelp['price_range']+'$'
+
+# Replace missing values with NaN
+DC_Yelp['price_range'].loc[swap] = np.nan
+
+DC_Yelp
+
+# Convert category into list of strings
+DC_Yelp['category'] = DC_Yelp.category.str.split(", ",expand=False)
+
+DC_Yelp
 
 
 
